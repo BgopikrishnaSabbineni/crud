acbbfe9c228fc6bc413bac157fc3768d6b3b73e8
BgopikrishnaSabbineni <gopikrishna2071997@gmail.com>
2020-08-08 13:57:36 -0400

    added

--- src/test/java/com/rest/crud/CrudApplicationTests.java
+++ src/test/java/com/rest/crud/CrudApplicationTests.java
@@ -3,7 +3,7 @@ package com.rest.crud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=CrudApplication.class)
 class CrudApplicationTests {
 
 	@Test
